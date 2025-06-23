1b1e90ccf0cf2e21ebd93642669a90c23bdb7b02
Josh Cummings <3627351+jzheaux@users.noreply.github.com>
2025-06-23 09:55:49 -0600

    Step 2: Client Certificate

--- gateway/src/main/java/com/example/gateway/GatewayApplication.java
+++ gateway/src/main/java/com/example/gateway/GatewayApplication.java
@@ -10,6 +10,7 @@
  import org.springframework.boot.autoconfigure.SpringBootApplication;
  import org.springframework.boot.web.client.RestClientCustomizer;
  import org.springframework.context.annotation.Bean;
+ import org.springframework.core.io.ClassPathResource;
  import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
  import org.springframework.stereotype.Controller;
  import org.springframework.web.bind.annotation.GetMapping;
@@ -26,7 +27,12 @@ class GatewayApplication {
    
     @Bean
     RestClientCustomizer tls() throws Exception {
+        ClassPathResource truststore = new ClassPathResource("certs/gateway-truststore.p12");
+        ClassPathResource keystore = new ClassPathResource("certs/gateway-keystore.p12");
+        char[] password = "password".toCharArray();
         SSLContext sslContext = SSLContexts.custom()
+            .loadKeyMaterial(keystore.getURL(), password, password)
+            .loadTrustMaterial(truststore.getURL(), password)
             .build();
 
         var connectionManager = PoolingHttpClientConnectionManagerBuilder.create()
